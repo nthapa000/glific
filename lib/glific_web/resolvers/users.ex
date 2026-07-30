7486fc0ce5d7f7465855b716e6064ce8fafd5e54
nthapa000 <nishantthapa0000@gmail.com>
2026-07-30 07:44:23 +0530

    fix

--- lib/glific_web/resolvers/users.ex
+++ lib/glific_web/resolvers/users.ex
@@ -37,7 +37,10 @@ defmodule GlificWeb.Resolvers.Users do
           {:ok, any} | {:error, any}
   def current_user(_, _, %{context: %{current_user: current_user}}) do
     with {:ok, user} <-
-           Repo.fetch_by(User, %{id: current_user.id, organization_id: current_user.organization_id}),
+           Repo.fetch_by(User, %{
+             id: current_user.id,
+             organization_id: current_user.organization_id
+           }),
          do: {:ok, %{user: user}}
   end
 
@@ -59,10 +62,11 @@ defmodule GlificWeb.Resolvers.Users do
 
   @spec update_password_params(User.t(), map()) :: {:ok, map()} | {:error, any}
   defp update_password_params(user, params) do
-    with false <- is_nil(params[:password]) || is_nil(params[:otp]),
-         :ok <- PasswordlessAuth.verify_code(user.phone, params.otp) do
+    with false <-
+           is_nil(params[:email]) && (is_nil(params[:password]) || is_nil(params[:otp])),
+         :ok <- PasswordlessAuth.verify_code(user.phone, params[:otp]) do
       PasswordlessAuth.remove_code(user.phone)
-      params = Map.merge(params, %{password_confirmation: params.password})
+      params = Map.merge(params, %{password_confirmation: params[:password]})
       {:ok, params}
     else
       true ->
